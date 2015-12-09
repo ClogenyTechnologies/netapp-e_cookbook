0c6747673315fd21ba3dbe7f7d4d6cd013b3049f
gaurav6351 <gaurav.harsola@msystechnologies.com>
2015-12-09 16:41:22 +0530

    Added optional parameters in recipes

--- recipes/mirror_group.rb
+++ recipes/mirror_group.rb
@@ -2,6 +2,11 @@
 netapp_e_mirror_group node['netapp']['mirror_group']['name']  do
   storage_system node['netapp']['storage_system_ip']
   secondary_array_id node['netapp']['mirror_group']['secondary_array_id']
+  sync_interval_minutes node['netapp']['mirror_group']['sync_interval_minutes']
+  manual_sync node['netapp']['mirror_group']['manual_sync']
+  recovery_warn_threshold_minutes node['netapp']['mirror_group']['recovery_warn_threshold_minutes']
+  repo_utilization_warn_threshold node['netapp']['mirror_group']['repo_utilization_warn_threshold']
+  syncWarn_threshold_minutes node['netapp']['mirror_group']['syncWarn_threshold_minutes']
   action :create
 end
 
